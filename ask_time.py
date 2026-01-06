8573734ed6b470c1afa0a079fde0a2760b4d96f5
arthur-georget <arthur.georget@laplateforme.io>
2026-01-06 10:47:10 +0100

    recopied files content to be able to merge everything

---
+++ ask_time.py
@@ -0,0 +1,12 @@
+def ask_time():
+    hour = 0
+    minute = 0
+    second = 0
+    hour = int(input("Veuillez entrer l'heure : "))
+    minute = int(input("Veuillez entrer les minutes : "))
+    second = int(input("Veuillez entrer les secondes : "))
+    return hour,minute,second
+
+#time = ask_time()
+#print(time)
+	
---
+++ display_current_time.py
@@ -0,0 +1,5 @@
+def display_current_time(current_time):
+    print(f"{current_time[0]}:{current_time[1]}:{current_time[2]}")
+
+#current_time=(20,59,23)
+#display_current_time(current_time)
