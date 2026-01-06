2e65f676488fff21475e6b235ff721fe46d2bd4e
arthur-georget <arthur.georget@laplateforme.io>
2026-01-06 12:01:01 +0100

    fix: set_alarm import file name changed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 from ask_time import ask_time
 from set_current_time import set_current_time
-from set_alarm_time import set_alarm_time
+from set_alarm import set_alarm_time
 from display_current_time import display_current_time
 from display_menu import display_menu
 from update_time import update_time
