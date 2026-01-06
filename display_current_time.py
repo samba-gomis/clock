6acc81abba03e2d7c10b58a1152459f01c78d0b8
arthur-georget <arthur.georget@laplateforme.io>
2026-01-06 14:06:06 +0100

    added hours minutes and seconds formatting when values under 10

--- display_current_time.py
+++ display_current_time.py
@@ -1,5 +1,18 @@
 def display_current_time(current_time):
-    print(f"{current_time[0]}:{current_time[1]}:{current_time[2]}")
+    if current_time[0] < 10:
+        hours = "0"+str(current_time[0])
+    else:
+        hours = str(current_time[0])
+    if current_time[1] < 10:
+        minutes = "0"+str(current_time[1])
+    else:
+        minutes = str(current_time[1])
+    if current_time[2] < 10:
+        seconds = "0"+str(current_time[2])
+    else:
+        seconds = str(current_time[2])
+
+    print(f"  {hours}:{minutes}:{seconds}", end='\r')
 
 #current_time=(20,59,23)
 #display_current_time(current_time)
